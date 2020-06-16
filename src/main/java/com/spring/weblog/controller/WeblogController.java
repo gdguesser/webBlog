e5d6f7478d7e4a120fc332511220d02587a8dcd7
gdguesser <gabrieldguesser@hotmail.com>
2020-06-16 20:59:59 -0300

    Criação do método getPostDetails

--- src/main/java/com/spring/weblog/controller/WeblogController.java
+++ src/main/java/com/spring/weblog/controller/WeblogController.java
@@ -5,6 +5,7 @@ import com.spring.weblog.repository.WeblogRepository;
 import com.spring.weblog.service.WeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,6 +23,14 @@ public class WeblogController {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = weblogService.findAll();
         mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = weblogService.findById(id);
+        mv.addObject("post", post);
         return mv;
     }
 
